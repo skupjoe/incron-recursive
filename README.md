9e0f4b09eca6271ce15d30121867f6d157c38353
Joseph Skupniewicz <joseph.skup@gmail.com>
2018-03-17 00:03:47 -0700

    Several updates

--- README.md
+++ README.md
@@ -3,21 +3,25 @@ incron-recursive
 
 What this script does:
   - Recursive monitoring with incron, for newly created subdirectories in a parent folder.
-  - Adds new watches to incrontab for newly created folders
-  - Updates incrontab when files/folders are arbitrarily renamed/removed [including folders with spaces]
-  - Add a .alternates folder to every newly created folder under the parent folder. 
-  - On IN_CREATE events [files are added], HandBrakeCLI & ffmpeg are called to create
+  - Adds new watches to incrontab for newly created folders.
+  - Updates incrontab when files/folders are arbitrarily renamed/removed (including files/folders with spaces, parentheses, and brackets).
+  - On IN_CREATE events (files are added), allows you to process these files with any arbitrary set of commands that you define.
+  - Performs sanity checks and cleanup tasks to ensure that incrontab is maintained up-to-date and recovers from race conditions.
 
-My incrontab entry looks something like this: 
+To use: make an intial update your incrontab entry to look like this: 
 
-/var/www/examplesite/projects IN_CREATE,IN_DELETE,IN_CLOSE_WRITE,IN_MOVED_TO /var/www/examplesite/update.py $# $@ $%
+/path/to/monitored/dir IN_CREATE,IN_DELETE,IN_CLOSE_WRITE,IN_MOVED_TO /path/to/update.py $# $@ $%
 
-If you want to use this script: 
-  - Change the variables scriptPath/curUser
+Also ensure that you: 
+  - Change the variables: scriptPath, curUser, updatePy
+  - Update the processFile() commands to your liking. You can add addition logic here if you so desire.
   - chmod +x the script
   - Write access the watched folder.
 
-The use case for this script is to monitor uploaded video files to a (watched) directory, and encode them to a webm and iPad format, for mobile viewing/HTML5 playback in Firefox. 
+Note: This script has been designed and will only work to monitor a +single+ root directory. It can be updated to handle more. Please share a new fork if you implement this!
+
+Lastly, shouts out to Thang for the original version of this script! (https://github.com/nguyent/incron-recursive)
+Brilliant work!
 
 Cheers,
-Thang
+Joseph
--- update.py
+++ update.py
@@ -3,37 +3,47 @@
 # '''
 # Any time a folder is added to the monitored parent directory, that folder needs
 # to be monitored as well [recursive monitoring is not fully supported]. This
-# script will add to incrontab when a new folder is added.
-# This script will also convert any files uploaded to a monitored folder
-# to a VP8 [webm] and mobile version to be put into an .alternates folder.
-# This script will also handle arbitrary file/folder renaming, at any depth.
+# script will add to incrontab when a new folder is added and process files that
+# are added to these directories with any arbitrary set of commands that you define.
+#
+# This script will also handle arbitrary file/folder deletions & renaming, at any 
+# depth.
 # '''
 
 import os,sys,pwd,subprocess, time
 from datetime import datetime
 
 # CFG Variables
-scriptPath = '/var/www/soundtrack/update.py' # the path to THIS script.
-changed = sys.argv[1].strip().replace(' ','\\ ') # spaces.
-if len(sys.argv) > 4:
-    workingDir = '\\ '.join(sys.argv[2:-1]) # spaces
-else:
-    workingDir = sys.argv[2] # no spaces
-event = sys.argv[-1]
-incrontemp = workingDir + '/temp'
-curUser = 'thang' # ensure this runs under the correct user, for incrontab
+curTime = datetime.time(datetime.now()).isoformat()
+curUser = 'root' # ensure this runs under the correct incrontab user
+scriptPath = '/path/to/update.py' # the path to THIS script.
+updatePy = '/path/to/monitored/dir IN_CREATE,IN_DELETE,IN_CLOSE_WRITE,IN_MOVED_TO %s $# $@ $%%' % scriptPath
+logDir = '/var/log/incrond'
+incrontemp = '/tmp/incrontemp'
 os.setuid(pwd.getpwnam(curUser)[2])
+event = sys.argv[-1]
+changed = sys.argv[1].strip().replace(' ','\\ ').replace('(','\(').replace(')','\)').replace('[','\\[').replace(']','\\]')
+if len(sys.argv) > 4:
+    workingDir = '\\ '.join(sys.argv[2:-1]).replace('(','\(').replace(')','\)').replace('[','\\[').replace(']','\\]')  # spaces
+else:
+    workingDir = sys.argv[2].replace(' ','\\ ').replace('(','\(').replace(')','\)').replace('[','\\[').replace(']','\\]')  # no spaces
+if not os.path.exists(logDir):
+    os.makedirs(logDir)
+
+def makeTemp():
+    if not os.path.isfile(incrontemp):
+        cmdList = ['/usr/bin/incrontab -l > %s' % incrontemp]
+        runCmd(cmdList)
 
 # Log to a timestamped log file
 def log(out, err, cmd):
-    curTime = datetime.time(datetime.now()).isoformat()
-    f = open(workingDir + '/error.%s.log' % curTime , 'w')
+    f = open(logDir + '/error.%s.log' % curTime , 'w')
     f.write('Could not update incrontab for %s' % changed + '\n')
     f.write('Failed output: \n')
     f.write('Attempted: %s \n' % cmd)
     f.write(out + '\n')
     f.write(err + '\n')
-    f.write('Parameters: %s, %s, %s' % (changed,workingDir,event) + '\n')
+    f.write('Parameters: %s, %s, %s' % (changed,logDir,event) + '\n')
     f.close()
 
 # Execute a list of shell commands, return the output as a list.
@@ -46,7 +56,6 @@ def runCmd(cmdList):
         if o.returncode != 0:
             log(out,err,cmd)
             sys.exit('Process failed..' )
-
     return outList
 
 # Update Incrontab after modifying the current incrontab.
@@ -56,59 +65,90 @@ def updateIncron(cmdList):
     # Replace with the changed incrontemp.
     cmdList += ['/usr/bin/incrontab %s' % incrontemp]
     runCmd(cmdList)
-    os.remove(incrontemp)
 
+def cleanIncron():
+    cmds = ['/usr/bin/incrontab -l']
+    incrontab = runCmd(cmds)[0]
+    for x in incrontab.split('\n')[:-1]:
+	tempDir = x.split(' IN_CREATE,')[0]
+        if not os.path.exists(tempDir.replace('\\ ', ' ')): 
+	    deletions = tempDir.replace('\\','\\\\').replace('/','\\/')
+	    cmds = ["sed -i '/%s\//d' %s" % (deletions, incrontemp)]
+            cmds += ["sed -i '/%s\\ /d' %s" % (deletions, incrontemp)]
+    updateIncron(cmds)
+	
 # Process an uploaded file.
 def processFile():
     # full path to the file..
     in_file = workingDir + '/' + changed
     # Pop off the extension.
     out_file = '.'.join(changed.split('.')[:-1])
-    # Put the file in the .alternates folder in the watched directory
-    out_file = workingDir+'/.alternates/' + out_file
-
-    # Make an iOS & Firefox playable version.
-    cmds = ["ffmpeg -y -i %s -threads 4 -f webm -vcodec libvpx  -filter:v yadif -g 120 -level 216 -profile 0 -qmax 42 -qmin 10 -rc_buf_aggressivity 0.95 -vb 2M -acodec libvorbis -aq 70 -ac 2 %s" % (in_file, out_file + '_ffox.webm' )]
-    cmds += ["HandBrakeCLI -i %s -o %s --preset=\"iPad\"" % ( in_file, out_file + '_ipad.mov' )]
+    # Fix ACL
+    cmds = ["setfacl -Rm m:rwx %s*" % (in_file.split('.')[0])]
+    # Do anything else you want
+    #cmds += ["HandBrakeCLI -i %s -o %s --preset=\"iPad\"" % ( in_file, out_file + '_ipad.mov' )]
     runCmd(cmds)
 
+# Fix duplicate and missing incrontab entries
+def removeDupsMissing():
+    seen = set()
+    cmds = ['/usr/bin/incrontab -l']
+    incrontab = runCmd(cmds)[0]
+    values = incrontab.split('\n')
+    f = open(incrontemp, "w")
+    for value in values:
+	watchedDir = value.split(' IN_CREATE,')[0].replace('\\','')
+        if os.path.exists(watchedDir) and value not in seen:
+            f.write(value+'\n')
+            seen.add(value)
+    f.close()
+    cmdList = ['/usr/bin/incrontab %s' % incrontemp]
+    runCmd(cmdList)
+
+# Fix race conditions
+def sanityCheck():
+    cmds = ['/usr/bin/incrontab -l']
+    incrontab = runCmd(cmds)[0]
+    trigger = incrontab.split('\n')[0]
+    # Replace trigger statement (update.py command) if this goes missing
+    if trigger != updatePy:
+	cmds = ["echo >> %s" % (incrontemp)]
+	cmds += ["sed -i '1s/^/%s\\n/' %s" % (updatePy.replace('\\','\\\\').replace('/','\\/'), incrontemp)]
+	cmds += ["sed -i '$ d' %s" % (incrontemp)]
+        cmds += ['/usr/bin/incrontab %s' % incrontemp]
+        runCmd(cmds)
+
 def main():
+
     # A Sub-Directory is added.
     if 'IN_CREATE,IN_ISDIR' in event:
         cmds = ["echo '%s IN_CREATE,IN_DELETE,IN_CLOSE_WRITE,IN_MOVED_TO %s $# $@ $%%' >> %s" % (workingDir+'/'+changed, scriptPath, incrontemp)]
-        # Add the .alternates folder, but don't monitor it.
-        cmds += ["mkdir %s" % (workingDir+'/'+changed+'/.alternates/')]
         # update incrontab to add/remove watches on the changed Directory
         updateIncron(cmds)
 
     # A Sub-Directory is deleted.
     elif 'IN_DELETE,IN_ISDIR' in event:
         deletions = (workingDir+'/'+changed).replace('\\','\\\\').replace('/','\\/')
-        # First we remove the exact entry, so something like foo/testx doesn't
-        # get removed when foo/test is deleted.
-        # Then, we remove instances of foo/test/ to take care of subdirs.
-
-        cmds = ["sed -i '/%s\\ /d' %s" % (deletions,incrontemp)]
-        cmds += ["sed -i '/%s\//d' %s" % (deletions, incrontemp)]
+        # First we remove the exact entry. Then we remove instances of foo/test/ to remove any subdirs.
+        cmds = ["sed -i '/%s\//d' %s" % (deletions, incrontemp)]
+        cmds += ["sed -i '/%s\\ /d' %s" % (deletions, incrontemp)]
         updateIncron(cmds)
+        sanityCheck()
 
     # A File is added.
-    elif 'IN_CLOSE_WRITE' in event:
+    elif 'IN_CREATE' in event and incrontemp not in changed:
         ext = changed.split('.')[-1]
-        # Only process valid movie files.
-        if 'mov' in ext:
-            processFile()
+        processFile()
 
     # A directory was renamed.
     elif 'IN_MOVED_TO,IN_ISDIR' in event:
-
         # Find the original name of the directory.
         # Compare with the directories in incrontab
         cmds = ['/usr/bin/incrontab -l']
-        cmdOut = runCmd(cmds)[0]
+        incrontab = runCmd(cmds)[0]
         oldName = ''
-
-        for x in cmdOut.split('\n')[:-1]:
+        
+        for x in incrontab.split('\n')[:-1]:
             tempDir = x.split(' IN_CREATE,')[0]
             # Ensure the basepath matches, and then check if it exists.
             # If it doesn't, we can assume that it was the renamed directory.
@@ -128,47 +168,20 @@ def main():
 
         oldName = oldName.replace('\\','\\\\').replace('/','\\/')
         newName = (workingDir+'/'+changed).replace('\\','\\\\').replace('/','\\/')
-        cmds = ["sed -i 's/%s /%s /g' %s" % (oldName,newName,incrontemp)]
-        updateIncron(cmds)
-
-    # A file was renamed.
-    elif 'IN_MOVED_TO' in event:
-        ch = ''.join(sys.argv[1].split('.')[:-1]) # remove the extension
-        wd = workingDir.replace('\\ ', ' ')
-        altFolder = wd + '/.alternates/'
-        files = os.walk(altFolder).next()[2]
-        # Pop off extensions & remove dupes.
-        files = list(set(map(lambda x: '_'.join(x.split('_')[:-1]),files)))
-        moved = ''
-        for f in files:
-            if not os.path.exists(wd+'/'+f+'.*'):
-                moved = f
-                break
-
-        try:
-            os.rename(altFolder + moved + '_ffox.webm',altFolder + ch + '_ffox.webm')
-            os.rename(altFolder + moved + '_ipad.mov',altFolder + ch + '_ipad.mov')
-
-        # If we can't rename the files, so either one of these happened:
-        # 1 - Only one of the alternates was created, in which case we still
-        # need to make the alternates again
-        # 2 - The file was deleted while this script was running, and we still
-        # need to make the alternates again.
-        except:
-            processFile()
-
-
-    # A file was removed.
-    elif 'IN_DELETE' in event:
-        altFolder = workingDir + '/.alternates/'
-
-        # Delete the alternates
-        try:
-            os.remove(altFolder + changed + '_ffox.webm')
-            os.remove(altFolder + changed + '_ipad.webm')
-        # Don't sweat if they don't exist.
-        except:
-            pass
+	if oldName:
+	    cmds = ["sed -i 's/%s /%s /g' %s" % (oldName,newName,incrontemp)]
+            updateIncron(cmds)
+	else:
+	    log("Attempting to update directory.", "Failed: no oldName match!", newName)
+	    cmds = ["echo '%s IN_CREATE,IN_DELETE,IN_CLOSE_WRITE,IN_MOVED_TO %s $# $@ $%%' >> %s" % (workingDir+'/'+changed, scriptPath, incrontemp)]
+	    updateIncron(cmds)
+	    cleanIncron()
+	sanityCheck()
 
 if __name__ == "__main__":
-    main()
+    try:
+	makeTemp()
+    	main()
+        removeDupsMissing()
+    except Exception as e:
+	log("Main block","Main block general exception: ", e)
